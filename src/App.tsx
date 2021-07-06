d2dddb81616d8e0ae70fffd0adf61439d7b19e37
fujiiree120 <fujiiree120@gmail.com>
2021-07-06 15:58:32 +0900

    タスク保存

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { FormControl, TextField } from '@material-ui/core';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import {db} from "./firebase";
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{id:"", title:""}]);
+  const [input, setInput] = useState("");
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot)=>{
       setTasks(
@@ -12,10 +15,23 @@ const App: React.FC = () => {
     });
     return () => unSub();
   }, []);
+
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>)=>{
+    db.collection("tasks").add({title: input});
+    setInput("");
+  }
   return (
     <div className="App">
-      {tasks.map((task)=><h3>{task.title}</h3>
-      )}
+      <h1>ToDo App</h1>
+      <FormControl>
+        <TextField label="New Task" value={input} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setInput(e.target.value)}/>
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon/>
+      </button>
+      {tasks.map((task)=>(
+      <h3 key={task.id}>{task.title}</h3>
+      ))}
     </div>
   );
 }
