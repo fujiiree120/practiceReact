04730a974a87584cf592144f3e8ada44fd0cb1ce
fujiiree120 <fujiiree120@gmail.com>
2021-08-14 16:16:24 +0900

    開業

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,80 @@
 import { FormControl, List, TextField } from '@material-ui/core';
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./App.module.css";
 import TaskItem from "./TaskItem";
-import {makeStyles} from "@material-ui/styles";
-import {db} from "./firebase";
+import { makeStyles } from "@material-ui/styles";
+import { db } from "./firebase";
+import { auth } from "./firebase";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const useStyles = makeStyles({
   field: {
     marginTop: 30,
     marginBottom: 20,
   },
-  list:{
+  list: {
     margin: "auto",
     width: "40%",
   },
 });
 
-const App: React.FC = () => {
-  const [tasks, setTasks] = useState([{id:"", title:""}]);
+const App: React.FC = (props: any) => {
+  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
   const [input, setInput] = useState("");
   const classes = useStyles();
+
   useEffect(() => {
-    const unSub = db.collection("tasks").onSnapshot((snapshot)=>{
+    const unSub = auth.onAuthStateChanged((user) => {
+      !user && props.history.push("login")
+    });
+    return () => unSub();
+  });
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
-        snapshot.docs.map((doc)=> ({id: doc.id, title: doc.data().title}))
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
       );
     });
     return () => unSub();
   }, []);
 
-  const newTask = (e: React.MouseEvent<HTMLButtonElement>)=>{
-    db.collection("tasks").add({title: input});
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: input });
     setInput("");
   }
   return (
     <div className={styles.app__root}>
       <h1>ToDo App</h1>
+      <button
+        className={styles.app__logout}
+        onClick={
+          async () => {
+            try {
+              await auth.signOut();
+              props.history.push("login");
+            } catch (error) {
+              alert(error.message);
+            }
+          }
+        }
+      >
+        <ExitToAppIcon></ExitToAppIcon>
+      </button>
       <br></br>
+
+
       <FormControl>
-        <TextField className={classes.field} label="New Task" value={input} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setInput(e.target.value)}/>
+        <TextField className={classes.field} label="New Task" value={input} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)} />
       </FormControl>
       <button className={styles.app__icon} disabled={!input} onClick={newTask}>
-        <AddToPhotosIcon/>
+        <AddToPhotosIcon />
       </button>
-  
+
       <List className={classes.list}>
-      {tasks.map((task)=>(
-        <TaskItem key={task.id} id={task.id} title={task.title}/>
-      ))}
+        {tasks.map((task) => (
+          <TaskItem key={task.id} id={task.id} title={task.title} />
+        ))}
       </List>
     </div>
   );
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import styles from './Login.module.css';
+import {Button, FormControl, TextField, Typography} from "@material-ui/core";
+import {auth} from "./firebase";
 
- const Login = () => {
+
+ const Login: React.FC = (props: any) => {
+     const [isLogin, setIsLogin] =useState(true);
+     const [email, setEmail] = useState("");
+     const [password, setPassword] = useState("");
+
+     useEffect(()=>{
+         const unSub = auth.onAuthStateChanged((user)=>{
+             user && props.history.push("/");
+         });
+         return () => unSub();
+     }, [props.history]);
     return (
-        <div>
-            Login Component
+        <div className={styles.login__root}>
+        <h1>{isLogin ? "Login": "Register"}</h1>
+        <FormControl>
+                <TextField
+                    name="email"
+                    label="email"
+                    value={email}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>{
+                        setEmail(e.target.value);
+                    }}
+                    >
+
+                    </TextField>
+        </FormControl>
+        <br/>
+        <FormControl>
+            <TextField
+                      name="password"
+                      label="password"
+                      value={password}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>{
+                        setPassword(e.target.value);
+                    }}
+            ></TextField>
+        </FormControl>
+        <br/>
+        <Button
+            variant="contained" color="primary" size="small"
+            onClick={
+                isLogin ? async () =>{
+                    try{
+                        await auth.signInWithEmailAndPassword(email, password);
+                        props.history.push("/")
+                    }catch(error){
+                        alert(error.message)
+                    }
+                } : async () =>{
+                    try{
+                        await auth.createUserWithEmailAndPassword(email, password);
+                        props.history.push("/");
+                    }catch(error){
+                        alert(error.message);
+                    }
+                }
+            }
+        >
+            {isLogin ? "Login" : "Register"}
+        </Button>
+        <br/>
+        <Typography align="center">
+            <span onClick={() => setIsLogin(!isLogin)}>
+                {isLogin ? "create new account" : "Back to Login"}
+            </span>
+        </Typography>
         </div>
     )
 };
